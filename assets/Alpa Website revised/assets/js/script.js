10d010c5aa46a2c5168f029d26ef1372366d4cda
Alphaam <58554818+Alphaam@users.noreply.github.com>
2020-11-03 03:39:39 +0400

    REVISED WEBSITE

---
+++ assets/Alpa Website revised/assets/js/script.js
@@ -0,0 +1,17 @@
+ 
+
+ // changing color of text when clicked on
+ let onebutton = document.getElementById("note");
+
+ onebutton.addEventListener('onclick', ()=>{
+
+ 	onebutton.style.color='green'
+
+ 	})
+
+  onebutton.addEventListener('onmouseoutr', ()=>{
+
+ 	onebutton.style.color='black'
+
+ 	})
+
